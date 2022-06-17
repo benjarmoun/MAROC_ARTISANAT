843268e6d9f9bdf46709bf1a689810ad75e68968
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2022-06-17 09:17:07 +0100

    Create ProdsAdmin.vue

---
+++ front-app/src/components/ProdsAdmin.vue
@@ -0,0 +1,96 @@
+    <!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+
+  <!-- <tbody class="divide-y divide-gray-200 bg-white"> -->
+    <tr>
+      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+        <!-- <div class="flex items-center"> -->
+          <div class="h-32 w-32 flex-shrink-0">
+            <img class="h-32 w-32 rounded-md" :src='"../src/assets/"+this.picture' alt="" />
+          </div>
+          <!-- </div> -->
+        </td>
+          <td>
+            <div>  
+          <div class="ml-4">
+            <div class="font-medium text-gray-900">{{ name }}</div>
+            <!-- <div class="text-gray-500">{{ product.email }}</div> -->
+          </div>
+        </div>
+      </td>
+      <td class=" px-3 py-4 text-sm text-gray-500 ">
+        <div class="text-gray-900  ">{{fname}}</div>
+        <div class="text-gray-500">{{lname }}</div>
+      </td>
+      <!-- <td
+                            class="px-6 py-4 border-b border-gray-200 whitespace-nowrap"
+                          >
+                            <div class="text-sm leading-5 text-gray-900">
+                              {{ seller.fname}} 
+                            </div>
+                            <div class="text-sm leading-5 text-gray-500">
+                              {{ seller.lname }} 
+                            </div>
+                          </td> -->
+      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+        <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">{{ category }}</span>
+      </td>
+      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ price }}</td>
+      <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-6">
+        <button @click="this.delete(this.id)" class="text-red-600  hover:text-red-900 mx-1">Delete</button>
+      </td>
+    </tr>
+  <!-- </tbody> -->
+</template>
+
+<script>
+import store from '../store';
+// import store from "../store/index"
+import { mapActions } from 'vuex';
+import axios from 'axios';
+
+export default {
+  name: 'Store',
+    props: {
+        picture: String,
+        price:  Number,
+        name: String,
+        description: String,
+        fname: String,
+        lname: String,
+        category: String,
+        id: Number
+    },
+    data() {
+        return {
+            // products: [],
+            // seller_id: '',
+            // categName: '',
+            // categs: [
+            //   {
+            //     id: 7,
+            //     name: 'Electronics'
+            //   },
+            //   {
+            //     id: 2,
+            //     name: 'Clothes'
+            //   }
+            // ],
+        }
+    },
+    methods: {
+      async delete(id) {
+        console.log(id);
+        let result = await axios.delete('http://localhost/MAROC_ARTISANAT/back-app/deleteProduct/'+id);
+        this.$parent.getProdsAdmin();
+
+        console.log(result);
+      }
+    },
+    mounted() {
+        // console.log(this.categs);
+        // this.categName = this.categs.find(categ => categ.id == this.category_id ? categ.name : 'no catge');
+    },
+
+}
+</script>
