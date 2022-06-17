9cb7f8a4311e329b97638ff8ea2c633719e3acc5
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2022-06-17 09:17:00 +0100

    Update App.vue

--- front-app/src/App.vue
+++ front-app/src/App.vue
@@ -1,14 +1,20 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
-<template>
+<template >
+<div>
 
   <div>
     <!-- <Header /> -->
   <RouterView />
+
   </div>
+  <!-- <div>
+    <Footer />
+  </div> -->
+</div>
 </template>
 
 <style>
